e92fa80f7d9e3751be57c6b69525495cba4ed613
juani <juaninv@outlook.com>
2016-12-02 11:18:06 -0300

    listo

--- node.js
+++ node.js
@@ -13,14 +13,23 @@ server.on("request", function(req,res){
             fs.readFile(filePath, function(err,data){
                 if (err){
                     res.writeHead(500);
-                    res.end("Ha ocurrido algo malo")
+                    if (path=="/"){
+                        filePath = "public/index.html";
+                        fs.readFile(filePath, function(err,data){
+                                res.end(data);
+                        })
+                    }
                 }else{
                     res.end(data);
                 }
             })
         }else {
             res.writeHead(404);
-            res.end("No existe")
+            filePath = "public/404.html";
+            fs.readFile(filePath, function(err,data){
+                res.end(data);
+            })
+
         }
     });
 });
